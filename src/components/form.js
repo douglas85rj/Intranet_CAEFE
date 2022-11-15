02ca2e6c4d5ebaf35c872b6c9fd4eabd7b8d5cdb
douglas85rj <douglas85rj@gmail.com>
2022-11-15 18:55:18 -0300

    FORM - CSS UPDATE

--- src/components/form.js
+++ src/components/form.js
@@ -5,15 +5,24 @@ export function Form() {
     return (
     <div className="form-contato">
         <h1>Fale conosco</h1>
-<form className="conato" method="post" action="https://getform.io/f/97332049-19ab-4e2d-bd51-90d0accb9b30">
+        
+<form className="conato" method="post" action="https://getform.io/f/97332049-19ab-4e2d-bd51-90d0accb9b30"> 
+<div className="row">
 <input type="text" name="fullname" id="nome" placeholder="Nome" required className="field" minLength={2} maxLength={36}></input>
+</div>
+<div className="row">
 <input type="email" name="email" id="email" placeholder="email" required className="field"></input>
+</div>
+<div className="row">
 <input type="tel" name="tel" id="tel" placeholder="Telefone (21) 99999-9999" className="field" pattern="\([0-9][0-9]\) 9?([0-9]{4}-[0-9]{4})"></input>
-<textarea name="mensagem" id="mensagem" required rows={3} className="field" minLength={12} maxLength={256}></textarea>
+</div>
+<div className="row">
+<textarea name="mensagem" id="mensagem" required rows={3} className="field" minLength={12} maxLength={256} placeholder="Coloque seu texto com no mínimo 12 e no máximo 256 caracteres."></textarea>
+</div>
 <button type="submit" required className="submit-button">Enviar</button>
 </form>
 <style jsx>{`
-        .contact-form-container {
+        .form-contato{
           margin: 12px;
         }
         .contact-form {
@@ -37,11 +46,11 @@ export function Form() {
           margin-top: 12px;
         }
         .field:focus {
-          border-color: #009712;
+          border-color:#0000ff;
         }
         .submit-button {
           width: 100%;
-          background-color: #009712;
+          background-color:#0000ff;
           padding: 12px 18px;
           color: #fff;
           border: none;
