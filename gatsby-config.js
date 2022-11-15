3ad472bf5f123a99da45d10ff30fb1c449839c81
Eduardo Velho <eduardo.velho@icloud.com>
2022-11-15 15:03:11 -0300

    versions fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,21 +2,20 @@ module.exports = {
   siteMetadata: {
     siteUrl: "https://douglas85rj.github.io/Intranet_CAEFE/",
     title: "Intranet CAEFE",
-     pathPrefix: "/intranet_caefe",
+    pathPrefix: "/intranet_caefe",
   },
   plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    
-    
+    "gatsby-plugin-styled-jsx",
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
         path: `${__dirname}/blog`,
-      }
+      },
     },
     "gatsby-plugin-mdx",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
   ],
 };
